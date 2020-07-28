69b21d40910e8f6b90026e8fd2ff84323131a284
Kelvin de Miranda Barros <kmbcg@hotmail.com>
2020-07-28 17:07:15 -0300

    v.0.1.7
    - Adição da lib react-router-dom.
    - Adição de algumas rotas com uso do RRD.
    - Realocação do arquivo da home page.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+// Páginas:
+import App from './pages/Home/index.js';
+import CadastroVideo from './pages/cadastro/Video/index.js';
+const Pagina404 = () => (<div>Página não encontrada. {"(Erro 404)"}</div>)
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Switch>
+      <Route exact path='/' component={ App } />
+      <Route path='/cadastro/video' component={ CadastroVideo } />
+      <Route component={ Pagina404 } /> {/* Rota sem o path será carregada se não houver carregado nenhuma rota anterior, servindo para a rota 404 de página não encontrada. */}
+    </Switch>
+  </BrowserRouter>,
   document.getElementById('root')
 );
