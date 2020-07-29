4e437ad5755ab07439c1b0bc39b718fd45cfe785
Kelvin de Miranda Barros <kmbcg@hotmail.com>
2020-07-29 00:15:12 -0300

    v.0.1.11
    - Ajustes na página de cadastro de categoria.

--- frontend/src/pages/cadastro/Categoria/index.js
+++ frontend/src/pages/cadastro/Categoria/index.js
@@ -1,17 +1,32 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import PagesBasis from '../../../components/PagesBasis/index.js';
+import PageDefault from '../../../components/PageDefault';
 
-function CadastroCategoria () {
-    return (
-        <PagesBasis>
-            <h1>Cadastro de Categoria</h1>
+function CadastroCategoria() {
+  return (
+    <PageDefault>
+      <h1>Cadastro de Categoria</h1>
 
-            <Link to='/'>
-                Ir para Home Page
-            </Link>
-        </PagesBasis>
-    );
+      <form>
+
+        <label>
+          Nome da Categoria:
+          <input
+            type="text"
+          />
+        </label>
+
+        <button>
+          Cadastrar
+        </button>
+      </form>
+
+
+      <Link to="/">
+        Ir para home
+      </Link>
+    </PageDefault>
+  )
 }
 
 export default CadastroCategoria;
