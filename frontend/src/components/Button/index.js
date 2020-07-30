6848d26b8d30848b3e7355912f0029a5808d3eca
Kelvin de Miranda Barros <kmbcg@hotmail.com>
2020-07-30 03:35:47 -0300

    v.0.1.12
    - Componentização de partes repetidas na página de cadastro de categoria.
    - Reorganização de conteúdo em arquivos.

--- frontend/src/components/Button/index.js
+++ frontend/src/components/Button/index.js
@@ -2,19 +2,20 @@ import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 
 const Button = styled(Link)`
-    color: var(--white);
     border: 1px solid var(--white);
+    border-radius: 5px;
     box-sizing: border-box;
+    color: var(--white);
     cursor: pointer;
-    padding: 16px 24px;
+    display: inline-block;
     font-style: normal;
     font-weight: bold;
     font-size: 16px;
     outline: none;
-    border-radius: 5px;
+    padding: 16px 24px;
     text-decoration: none;
-    display: inline-block;
     transition: opacity .3s;
+    
     &: hover,
     &: focus {
         opacity: .5;
--- frontend/src/pages/cadastro/Categoria/index.js
+++ frontend/src/pages/cadastro/Categoria/index.js
@@ -1,26 +1,83 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import PageDefault from '../../../components/PageDefault';
+import PageDefault from '../../../components/PagesBasis/index.js';
+import FormField from '../../../components/FormField/index.js';
 
 function CadastroCategoria() {
+  const initialValues = {
+    name: '',
+    description: '',
+    color: '#000000'
+  }
+  const [ category, setCategory ] = useState( initialValues );
+  const [ categories, setCategories ] = useState( [] );
+  
+  function setValue ( key, value ) {
+    setCategory({
+      ...category,
+      [ key ]: value
+    });
+  }
+
+  function handleChange ( event ) {
+    setValue(
+      event.target.getAttribute('name'),
+      event.target.value
+    );
+  }
+
   return (
     <PageDefault>
-      <h1>Cadastro de Categoria</h1>
+      <h1>Cadastro de Categoria: { category.name }</h1>
 
-      <form>
+      <form onSubmit={ event => {
+        event.preventDefault();
+        setCategories( [ ...categories, category ] );
 
-        <label>
-          Nome da Categoria:
-          <input
-            type="text"
-          />
-        </label>
+        setCategory( initialValues );
+      }}>
+
+        <FormField 
+          labelText={ 'Nome da Categoria:' }
+          type={ 'text' }
+          value={ category.name }
+          name={ 'name' }
+          placeHolder={ 'Categoria' }
+          onChange={ handleChange }
+        />
+
+        <FormField
+          labelText={ 'Descrição:' }
+          type={ 'text' }
+          value={ category.description }
+          name={ 'description' }
+          placeHolder={ 'Descrição' }
+          onChange={ handleChange }
+        />
+        
+        <FormField
+          labelText={ 'Cor:' }
+          type={ 'color' }
+          value={ category.color }
+          name='color'
+          onChange={ handleChange }
+        />
 
         <button>
           Cadastrar
         </button>
       </form>
 
+      <ul>
+        { categories.map( (category, index) => {
+            return (
+              <li key={`${ index }`}>
+                { category.name }
+              </li>
+            );
+          })
+        }
+      </ul>
 
       <Link to="/">
         Ir para home
